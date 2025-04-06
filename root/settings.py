714774b9314e3f29e0a3f5c8369ec74eadb520cd
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-04-06 11:23:38 -0600

    corregir la configuración de STATICFILES_DIR y STATIC_ROOT en settings.py

--- root/settings.py
+++ root/settings.py
@@ -127,12 +127,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIR = [
+STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
 
 if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATIC_ROOT = BASE_DIR / 'staticfiles'
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
