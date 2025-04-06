0670e610adde4233535887bab84f3da2c0fb27cd
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-04-06 11:13:26 -0600

    añadir ruta para la vista de partidas en el archivo de URLs

--- root/urls.py
+++ root/urls.py
@@ -21,4 +21,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("home.urls")),
+    path('partidas/', include('partidas.urls'))
 ]
