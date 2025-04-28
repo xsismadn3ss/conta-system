ba719a25f6544a1542a00b03e19a9f3d4a81e266
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-04-28 00:05:52 -0600

    feat: add catalogo app URL routing
    This commit introduces a new URL routing configuration for the `catalogo` app, enabling it to be accessed via the `/catalogo/` endpoint. This change is necessary to integrate the `catalogo` app into the project's URL structure.

--- root/urls.py
+++ root/urls.py
@@ -21,5 +21,6 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("home.urls")),
-    path('partidas/', include('partidas.urls'))
+    path('partidas/', include('partidas.urls')),
+    path('catalogo/', include('catalogo.urls')),
 ]
