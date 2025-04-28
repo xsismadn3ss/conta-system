288b3afeaba880540a2ea836c1ff968da40f03e7
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-04-28 14:03:44 -0600

    feat(libros): implement CRUD views for LibroMayor and Tipo models

--- libros/views.py
+++ libros/views.py
@@ -1,3 +1,97 @@
 from django.shortcuts import render
+from django.views.generic import ListView, CreateView
+from django.urls import reverse_lazy
+from django.shortcuts import redirect
+from django.http.request import HttpRequest
+from .models import LibroMayor, Tipo
+from .forms import LibroMayorForm, TipoForm
 
-# Create your views here.
+
+# Libros Mayores inicio
+def index(request:HttpRequest):
+    return render(request, "libros_index.html")
+
+class LibroMayorListView(ListView):
+    model = LibroMayor
+    template_name = "libros/libros_list.html"
+    context_object_name = "libros"
+
+class LibroMayorCreateView(CreateView):
+    model = LibroMayor
+    template_name = "libros/libros_form.html"
+    fields = ["saldo_anterior", "movimiento", "saldo_final", "tipo_movimiento", "cuenta"]
+    success_url = reverse_lazy("libros:libros_list")
+
+def libro_detail_view(request:HttpRequest, pk:int):
+    try:
+        libro = LibroMayor.objects.get(pk=pk)
+        return render(request, "libros/libros_detail.html", {"libro": libro})
+    except LibroMayor.DoesNotExist:
+        return render(request, "libros/libros_not_found.html")
+
+def libro_update_view(request:HttpRequest, pk:int):
+    try:
+        libro = LibroMayor.objects.get(pk=pk)
+        if request.method == "POST":
+            form = LibroMayorForm(request.POST, instance=libro)
+            if form.is_valid():
+                form.save()
+                return redirect("libros:libros_list")
+        else:
+            form = LibroMayorForm(instance=libro)
+        return render(request, "libros/libros_form.html", {"form": form})
+    except LibroMayor.DoesNotExist:
+        return render(request, "libros/libros_not_found.html")
+
+def libro_delete_view(request:HttpRequest, pk:int):
+    try:
+        libro = LibroMayor.objects.get(pk=pk)
+        if request.method == "POST":
+            libro.delete()
+            return redirect("libros:libros_list")
+        return render(request, "libros/libros_confirm_delete.html", {"libro": libro})
+    except LibroMayor.DoesNotExist:
+        return render(request, "libros/libros_not_found.html")
+
+# Tipo views
+class TipoListView(ListView):
+    model = Tipo
+    template_name = "tipo/tipo_list.html"
+    context_object_name = "tipos"
+
+class TipoCreateView(CreateView):
+    model = Tipo
+    template_name = "tipo/tipo_form.html"
+    fields = ["nombre"]
+    success_url = reverse_lazy("libros:tipo_list")
+
+def tipo_detail_view(request:HttpRequest, pk:int):
+    try:
+        tipo = Tipo.objects.get(pk=pk)
+        return render(request, "tipo/tipo_detail.html", {"tipo": tipo})
+    except Tipo.DoesNotExist:
+        return render(request, "tipo/tipo_not_found.html")
+
+def tipo_update_view(request:HttpRequest, pk:int):
+    try:
+        tipo = Tipo.objects.get(pk=pk)
+        if request.method == "POST":
+            form = TipoForm(request.POST, instance=tipo)
+            if form.is_valid():
+                form.save()
+                return redirect("libros:tipo_list")
+        else:
+            form = TipoForm(instance=tipo)
+        return render(request, "tipo/tipo_form.html", {"form": form})
+    except Tipo.DoesNotExist:
+        return render(request, "tipo/tipo_not_found.html")
+
+def tipo_delete_view(request:HttpRequest, pk:int):
+    try:
+        tipo = Tipo.objects.get(pk=pk)
+        if request.method == "POST":
+            tipo.delete()
+            return redirect("libros:tipo_list")
+        return render(request, "tipo/tipo_confirm_delete.html", {"tipo": tipo})
+    except Tipo.DoesNotExist:
+        return render(request, "tipo/tipo_not_found.html")
