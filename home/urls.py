0ac760843f766ad903725a701fade8fae41bf0b8
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-04-29 07:53:54 -0600

    feat(authentication): add login and logout URLs to home app
    Introduce new URL patterns for user authentication within the home app. This allows users to access login and logout functionality directly from the home URLs, improving the user experience and streamlining authentication processes.

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", view=views.index, name="home"),
+    path('', view=views.index, name='home'),
+    path('accounts/login/', views.login_view, name='login'),
+    path('accounts/logout/', views.logout_view, name='logout'),
 ]
