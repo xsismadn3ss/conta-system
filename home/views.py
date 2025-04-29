86abe30af2fb9eaaa8e384d631a9fbdb25f12015
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-04-29 07:53:35 -0600

    feat(auth): add login and logout functionality to home views
    Implement login and logout views with authentication checks and redirects. The login view handles POST requests for user authentication, while the logout view ensures users are logged out securely. Both views use Django's built-in authentication and decorators for enhanced security.

--- home/views.py
+++ home/views.py
@@ -1,6 +1,31 @@
 from django.shortcuts import render
+from django.contrib.auth import login, logout, authenticate
+from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
+from django.http.request import HttpRequest
 
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', status=200)
+
+
+def login_view(request: HttpRequest):
+    if request.user.is_authenticated:
+        return redirect('home')
+        
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Invalid username or password')
+    return render(request, 'login.html')
+
+@login_required
+def logout_view(request: HttpRequest):
+    logout(request)
+    return redirect('login')
