6453062aad40cb538bc499670d1bf70b5312576b
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-04-29 18:17:53 -0600

    refactor(models): make saldo_final non-editable and auto-calculate in save
    The saldo_final field is now non-editable and is automatically calculated during the save method to ensure consistency and prevent manual errors. This improves data integrity by enforcing the correct calculation of saldo_final based on saldo_anterior and movimiento.

--- libros/models.py
+++ libros/models.py
@@ -15,10 +15,15 @@ class Tipo(models.Model):
 class LibroMayor(models.Model):
     saldo_anterior = models.FloatField()
     movimiento = models.FloatField()
-    saldo_final = models.FloatField()
+    saldo_final = models.FloatField(editable=False)  # No editable manualmente
     
     tipo_movimiento = models.ForeignKey(Tipo, on_delete=models.CASCADE)
     cuenta = models.ForeignKey("catalogo.Cuenta", on_delete=models.CASCADE, default=None, null=True)
+
+    def save(self, *args, **kwargs):
+        # Calcula el saldo final automáticamente
+        self.saldo_final = self.saldo_anterior + self.movimiento
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return "saldo anterior: {}, movimiento: {}, saldo final: {}".format(
