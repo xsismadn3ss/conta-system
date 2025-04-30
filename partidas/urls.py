855f11e9ef5569cd7b82462f27c8c6877c27e817
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-04-29 19:54:04 -0600

    refactor(urls): simplify and restructure URL patterns for partidas and detalles
    Consolidate URL patterns by removing redundant paths and nesting detalles routes under partidas for better organization and clarity. This improves maintainability and aligns with the hierarchical relationship between partidas and detalles.

--- partidas/urls.py
+++ partidas/urls.py
@@ -4,20 +4,17 @@ from . import views
 app_name = 'partidas'
 
 urlpatterns = [
-    # inicio
-    path('', view=views.index, name='index'),
-
     # partidas
-    path('partidas/', view=views.PartidaListView.as_view(), name='partida_list'),
-    path('partidas/nueva/', view=views.PartidaCreateView.as_view(), name='partida_create'),
-    path('partidas/<int:pk>', view=views.partida_detail_view, name='partida_detail'),
-    path('partidas/<int:pk>/editar', view=views.partida_update_view, name='partida_update'),
-    path('partidas/<int:pk>/eliminar', view=views.jerarquia_delete_view, name='partida_delete'),
+    path('', view=views.partida_list_view, name='partida_list'),
+    path('nueva/', view=views.partida_create_view, name='partida_create'),
+    path('<int:pk>', view=views.partida_detail_view, name='partida_detail'),
+    path('<int:pk>/editar', view=views.partida_update_view, name='partida_update'),
+    path('<int:pk>/eliminar', view=views.partida_delete_view, name='partida_delete'),
 
     # detalles
-    path('detalles/', view=views.detalles_list_view, name='detalles_list'),
-    path('detalles/nueva/', view=views.DetallesCreateView.as_view(), name='detalles_create'),
-    path('detalles/<int:pk>', view=views.detalles_detail_view, name='detalles_detail'),
-    path('detalles/<int:pk>/editar', view=views.detalles_update_view, name='detalles_update'),
-    path('detalles/<int:pk>/eliminar', view=views.detalles_delete_view, name='detalles_delete')
+    path('<int:pk>/detalles/nueva/', view=views.detalles_create_view, name='detalles_create'),
+    path('<int:pk>/detalles/<int:detalle_id>', view=views.detalles_detail_view, name='detalles_detail'),
+    path('<int:pk>/detalles/<int:detalle_id>/editar', view=views.detalles_update_view, name='detalles_update'),
+    path('<int:pk>/detalles/<int:detalle_id>/eliminar', view=views.detalles_delete_view, name='detalles_delete'),
+    path('<int:pk>/detalles', view=views.detalles_by_partida, name='partida_detalles'),
 ]
